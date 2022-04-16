c39c4f231f6f06180fea2dba05645edeaf6c740f
Adkinsm <76773401+adkinsmpage@users.noreply.github.com>
2022-04-16 08:55:55 +0800

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,22 +1,36 @@
 module.exports = {
     title: "Scadies",
     description: "快速、简便、安全的新一代动态博客系统",
+    plugins: [
+        '@vuepress/active-header-links',   // 页面滚动时自动激活侧边栏链接的插件
+        '@vuepress/back-to-top',          // 返回顶部插件
+        '@vuepress/medium-zoom',          // 图片预览插件
+        '@vuepress/nprogress',        //页面顶部进度条
+    ],
     themeConfig: {
+        lastUpdated: 'Last Updated', // string | boolean
+        smoothScroll: true,
         // 导航栏配置
         nav: [
             { text: "Home", link: "/" },
             { text: "Guide", link: "/guide/" },
         ], //  左侧导航栏配置
-        sidebar: {
-            "scadies": [
-                {
-                    text: "Scadies",
-                    children: [
-                        { text: "Get Started", link: "/guide/" },
-                        { text: "Config", link: "/htmldocs/config" },
-                    ],
-                },
-            ],
-        },
+        sidebar:{
+            '/': getSidebar()
+        }
     },
 };
+
+
+function getSidebar() {
+    return [
+        {
+            text:'Scadies',
+            children: [
+                { text: 'Get Started', link: '/guide/' },
+                { text: 'Config', link: '/guide/config' },
+            ],
+            sidebarDepth:3
+        },
+    ]
+}
