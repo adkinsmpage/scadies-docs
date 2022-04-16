f8bb9c66c5ebee7fc1af02953e0175070d8793bf
Adkinsm <76773401+adkinsmpage@users.noreply.github.com>
2022-04-16 08:39:14 +0800

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,10 +1,22 @@
 module.exports = {
-  title: 'Scadies',
-  description: '快速、简便、安全的新一代动态博客系统',
-   themeConfig: {
-    sidebar: [
-      '/guide',
-      '/guide/config'
-    ]
-  }
-}
+    title: "Scadies",
+    description: "快速、简便、安全的新一代动态博客系统",
+    themeConfig: {
+        // 导航栏配置
+        nav: [
+            { text: "Home", link: "/" },
+            { text: "Guide", link: "/guide/" },
+        ], //  左侧导航栏配置
+        sidebar: {
+            scadies: [
+                {
+                    text: "Scadies",
+                    children: [
+                        { text: "Get Started", link: "/guide/" },
+                        { text: "Config", link: "/htmldocs/config" },
+                    ],
+                },
+            ],
+        },
+    },
+};
